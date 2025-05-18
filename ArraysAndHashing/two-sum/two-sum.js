76a012b8229fed975dfd5d94a71fad4aa481cbde
sava-stanchev <s.stanchev94@gmail.com>
2025-05-18 21:29:31 +0300

    two sum

---
+++ ArraysAndHashing/two-sum/two-sum.js
@@ -0,0 +1,19 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+var twoSum = function (nums, target) {
+  let prevMap = new Map();
+
+  for (let i = 0; i < nums.length; i++) {
+    const diff = target - nums[i];
+
+    if (prevMap.has(diff)) {
+      return [prevMap.get(diff), i];
+    } else {
+      prevMap.set(nums[i], i);
+    }
+  }
+  return [];
+};
---
+++ ArraysAndHashing/two-sum/two-sum.md
@@ -0,0 +1,5 @@
+https://leetcode.com/problems/two-sum/
+
+Time O(n) | Space O(n)
+
+use hash map to instantly check for difference value, map will add index of last occurrence of a num, don't use same element twice
