784cdddee8c0347b9f5c9f252fa196838e5d1141
sava-stanchev <s.stanchev94@gmail.com>
2025-05-18 20:54:49 +0300

    valid anagram

--- ArraysAndHashing/valid-anagram/valid-anagram.js
+++ ArraysAndHashing/valid-anagram/valid-anagram.js
@@ -1,11 +1,4 @@
 /**
- * https://leetcode.com/problems/valid-anagram/
- *
- * Time O(n) | Space O(1)
- *
- * hash map to count each char in str1,
- * decrement for str2
- *
  * @param {string} s
  * @param {string} t
  * @return {boolean}
---
+++ ArraysAndHashing/valid-anagram/valid-anagram.md
@@ -0,0 +1,5 @@
+https://leetcode.com/problems/valid-anagram/
+
+Time O(n) | Space O(1)
+
+hash map to count each char in str1, decrement for str2
