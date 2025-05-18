92c7b0755f2c8a8e30bdca336b17de4fcb789bc4
sava-stanchev <s.stanchev94@gmail.com>
2025-05-18 20:54:36 +0300

    remove comments from contains duplicate cpp solution

--- ArraysAndHashing/contains-duplicate/contains-duplicate.cpp
+++ ArraysAndHashing/contains-duplicate/contains-duplicate.cpp
@@ -1,11 +1,3 @@
-/**
- * https://leetcode.com/problems/contains-duplicate/
- *
- * Time O(n) | Space O(n)
- *
- * hash set to get unique values in array,
- * to check for duplicates easily
- */
 #include <vector>
 #include <unordered_set>
 using namespace std;
