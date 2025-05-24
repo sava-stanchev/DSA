9fa3a41436f1cd62f494e92066d81b3b33672bc9
sava-stanchev <s.stanchev94@gmail.com>
2025-05-24 20:18:17 +0300

    valid palindrome

---
+++ TwoPointers/valid-palindrome/valid-palindrome.js
@@ -0,0 +1,35 @@
+/**
+ * @param {string} s
+ * @return {boolean}
+ */
+var isPalindrome = function (s) {
+    let l = 0;
+    let r = s.length - 1;
+
+    while (l < r) {
+        while (l < r && !alphaNum(s[l])) {
+            l++;
+        }
+
+        while (r > l && !alphaNum(s[r])) {
+            r--;
+        }
+
+        if (s[l].toLowerCase() !== s[r].toLowerCase()) {
+            return false;
+        }
+
+        l++;
+        r--;
+    }
+
+    return true;
+};
+
+function alphaNum(c) {
+    return (
+        (c >= "A" && c <= "Z") ||
+        (c >= "a" && c <= "z") ||
+        (c >= "0" && c <= "9")
+    );
+}
---
+++ TwoPointers/valid-palindrome/valid-palindrome.md
@@ -0,0 +1,5 @@
+https://leetcode.com/problems/valid-palindrome/
+
+Time O(n) | Space O(1)
+
+left, right pointers, update left and right until each points at alphanum, compare left and right, continue until left >= right, don’t distinguish between upper/lowercase;
