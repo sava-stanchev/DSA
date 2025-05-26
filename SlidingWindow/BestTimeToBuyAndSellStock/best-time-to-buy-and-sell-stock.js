23f24088e31fcbaceb924b091d4c594b14e216a0
sava-stanchev <s.stanchev94@gmail.com>
2025-05-26 11:46:42 +0300

    best time to buy and sell stock

--- SlidingWindow/BestTimeToBuyAndSellStock/best-time-to-buy-and-sell-stock.js
+++ SlidingWindow/BestTimeToBuyAndSellStock/best-time-to-buy-and-sell-stock.js
@@ -1,10 +1,4 @@
 /**
- * https://leetcode.com/problems/best-time-to-buy-and-sell-stock/
- *
- * Time O(n) | Space O(1)
- *
- * find local min and search for local max, sliding window;
- *
  * @param {number[]} prices
  * @return {number}
  */
---
+++ SlidingWindow/BestTimeToBuyAndSellStock/best-time-to-buy-and-sell-stock.md
@@ -0,0 +1,5 @@
+https://leetcode.com/problems/best-time-to-buy-and-sell-stock/
+
+Time O(n) | Space O(1)
+
+find local min and search for local max, sliding window;
