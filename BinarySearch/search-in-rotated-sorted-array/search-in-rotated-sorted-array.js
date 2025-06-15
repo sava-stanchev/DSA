7d3117385484c3955fecbec2f34ab00e1fdc859c
sava-stanchev <s.stanchev94@gmail.com>
2025-06-15 21:25:05 +0300

    search in rotated sorted array

---
+++ BinarySearch/search-in-rotated-sorted-array/search-in-rotated-sorted-array.js
@@ -0,0 +1,30 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number}
+ */
+var search = function (nums, target) {
+    let l = 0;
+    let r = nums.length - 1;
+
+    while (l <= r) {
+        const mid = Math.floor((l + r) / 2);
+        if (target === nums[mid]) return mid;
+
+        if (nums[l] <= nums[mid]) {
+            if (target > nums[mid] || target < nums[l]) {
+                l = mid + 1;
+            } else {
+                r = mid - 1;
+            }
+        } else {
+            if (target < nums[mid] || target > nums[r]) {
+                r = mid - 1;
+            } else {
+                l = mid + 1;
+            }
+        }
+    }
+
+    return -1;
+};
---
+++ BinarySearch/search-in-rotated-sorted-array/search-in-rotated-sorted-array.md
@@ -0,0 +1,5 @@
+https://leetcode.com/problems/search-in-rotated-sorted-array/
+
+Time O(logn) | Space O(1)
+
+binary search with rotated array logic, identify sorted half
