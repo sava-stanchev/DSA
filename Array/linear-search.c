af6b2a0d7ca5be2b737cad7ab8f6a6f7e7d87cda
sava-stanchev <s.stanchev94@gmail.com>
2025-07-22 23:37:25 +0300

    array - linear search

---
+++ Array/linear-search.c
@@ -0,0 +1,49 @@
+#include <stdio.h>
+
+struct Array
+{
+    int A[10];
+    int size;
+    int length;
+};
+
+void Display(struct Array arr)
+{
+    int i;
+    printf("\nElements are\n");
+    for (i = 0; i < arr.length; i++)
+        printf("%d ", arr.A[i]);
+}
+
+void swap(int *x, int *y)
+{
+    int temp;
+    temp = *x;
+    *x = *y;
+    *y = temp;
+}
+
+int LinearSearch(struct Array *arr, int key)
+{
+    int i;
+    for (i = 0; i < arr->length; i++)
+    {
+        if (key == arr->A[i])
+        {
+            swap(&arr->A[i], &arr->A[0]);
+            return i;
+        }
+    }
+
+    return -1;
+}
+
+int main()
+{
+    struct Array arr = {{2, 3, 4, 5, 6}, 10, 5};
+
+    printf("%d\n", LinearSearch(&arr, 5));
+    Display(arr);
+
+    return 0;
+}
