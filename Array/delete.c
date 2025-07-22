8cadd150267740aeb36e46c12368ac4745533a81
sava-stanchev <s.stanchev94@gmail.com>
2025-07-22 20:49:58 +0300

    array - delete

---
+++ Array/delete.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+
+struct Array
+{
+    int A[10];
+    int size;
+    int length;
+};
+
+void Display(struct Array arr)
+{
+    int i;
+    printf("\nElements are\n");
+    for (i = 0; i < arr.length; i++)
+        printf("%d ", arr.A[i]);
+}
+
+int Delete(struct Array *arr, int index)
+{
+    int x = 0;
+    int i;
+
+    if (index >= 0 && index < arr->length)
+    {
+        x = arr->A[index];
+        for (i = index; i < arr->length - 1; i++)
+            arr->A[i] = arr->A[i + 1];
+
+        arr->length--;
+        return x;
+    }
+
+    return 0;
+}
+
+int main()
+{
+    struct Array arr = {{2, 3, 4, 5, 6}, 10, 5};
+
+    printf("%d\n", Delete(&arr, 3));
+    Display(arr);
+
+    return 0;
+}
