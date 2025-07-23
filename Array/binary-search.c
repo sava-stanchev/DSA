31c3579c2be9aaa71082c299e32d819b497d7803
sava-stanchev <s.stanchev94@gmail.com>
2025-07-23 23:16:08 +0300

    array - binary search

---
+++ Array/binary-search.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+
+struct Array
+{
+    int A[10];
+    int size;
+    int length;
+};
+
+void Display(struct Array arr)
+{
+    int i;
+    printf("\nElements are\n");
+    for (i = 0; i < arr.length; i++)
+        printf("%d ", arr.A[i]);
+}
+
+int BinarySearch(struct Array arr, int key)
+{
+    int l, mid, h;
+    l = 0;
+    h = arr.length - 1;
+
+    while (l <= h)
+    {
+        mid = (l + h) / 2;
+
+        if (key == arr.A[mid])
+            return mid;
+        else if (key < arr.A[mid])
+            h = mid - 1;
+        else
+            l = mid + 1;
+    }
+
+    return -1;
+}
+
+int main()
+{
+    struct Array arr = {{2, 3, 4, 5, 6}, 10, 5};
+
+    printf("%d\n", BinarySearch(arr, 5));
+    Display(arr);
+
+    return 0;
+}
