02ac725cb33171d596816a0c99e4823180255fdd
sava-stanchev <s.stanchev94@gmail.com>
2025-07-23 23:15:52 +0300

    array - linear search - remove swap to front

--- Array/linear-search.c
+++ Array/linear-search.c
@@ -15,24 +15,13 @@ void Display(struct Array arr)
         printf("%d ", arr.A[i]);
 }
 
-void swap(int *x, int *y)
-{
-    int temp;
-    temp = *x;
-    *x = *y;
-    *y = temp;
-}
-
-int LinearSearch(struct Array *arr, int key)
+int LinearSearch(struct Array arr, int key)
 {
     int i;
-    for (i = 0; i < arr->length; i++)
+    for (i = 0; i < arr.length; i++)
     {
-        if (key == arr->A[i])
-        {
-            swap(&arr->A[i], &arr->A[0]);
+        if (key == arr.A[i])
             return i;
-        }
     }
 
     return -1;
@@ -42,7 +31,7 @@ int main()
 {
     struct Array arr = {{2, 3, 4, 5, 6}, 10, 5};
 
-    printf("%d\n", LinearSearch(&arr, 5));
+    printf("%d\n", LinearSearch(arr, 5));
     Display(arr);
 
     return 0;
