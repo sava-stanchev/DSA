83f1aa8012613661b75a45cfe0bef06fa18c1635
sava-stanchev <s.stanchev94@gmail.com>
2025-08-06 22:21:39 +0300

    linked list - remove duplicates from sorted

---
+++ Linked-List/remove-duplicates-from-sorted-linked-list.c
@@ -0,0 +1,67 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Node
+{
+    int data;
+    struct Node *next;
+} *first = NULL;
+
+void create(int A[], int n)
+{
+    int i;
+    struct Node *t, *last;
+    first = (struct Node *)malloc(sizeof(struct Node));
+    first->data = A[0];
+    first->next = NULL;
+    last = first;
+
+    for (i = 1; i < n; i++)
+    {
+        t = (struct Node *)malloc(sizeof(struct Node));
+        t->data = A[i];
+        t->next = NULL;
+        last->next = t;
+        last = t;
+    }
+}
+
+void Display(struct Node *p)
+{
+    while (p != NULL)
+    {
+        printf("%d ", p->data);
+        p = p->next;
+    }
+}
+
+void RemoveDuplicate(struct Node *p)
+{
+    struct Node *q = p->next;
+
+    while (q != NULL)
+    {
+        if (p->data != q->data)
+        {
+            p = q;
+            q = q->next;
+        }
+        else
+        {
+            p->next = q->next;
+            free(q);
+            q = p->next;
+        }
+    }
+}
+
+int main()
+{
+    int A[] = {10, 20, 20, 20, 30, 40, 40, 50};
+    create(A, 8);
+
+    RemoveDuplicate(first);
+    Display(first);
+
+    return 0;
+}
