d8939818be9d539dfd6d714cb0da4a05c4b37dad
sava-stanchev <s.stanchev94@gmail.com>
2025-09-23 21:37:34 +0300

    shell sort

---
+++ Sorting-Techniques/shell-sort.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+void ShellSort(int A[], int n)
+{
+    int gap, i, j, temp;
+
+    for (gap = n / 2; gap >= 1; gap /= 2)
+    {
+        for (i = gap; i < n; i++)
+        {
+            temp = A[i];
+            j = i - gap;
+
+            while (j >= 0 && A[j] > temp)
+            {
+                A[j + gap] = A[j];
+                j = j - gap;
+            }
+
+            A[j + gap] = temp;
+        }
+    }
+}
+
+int main()
+{
+    int A[] = {11, 13, 7, 12, 16, 9, 24, 5, 10, 3}, n = 10, i;
+
+    ShellSort(A, n);
+
+    for (i = 0; i < n; i++)
+        printf("%d ", A[i]);
+    printf("\n");
+
+    return 0;
+}
