0885c3a7b45bdab541964cf8cb8ed92d993fdb33
sava-stanchev <s.stanchev94@gmail.com>
2025-09-24 19:25:27 +0300

    hashing - chaining

---
+++ Hashing-Techniques/chaining/chaining.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "chains.h"
+
+int hash(int key)
+{
+    return key % 10;
+}
+
+void Insert(struct Node *H[], int key)
+{
+    int index = hash(key);
+    SortedInsert(&H[index], key);
+}
+
+int main()
+{
+    struct Node *HT[10];
+    struct Node *temp;
+    int i;
+
+    for (i = 0; i < 10; i++)
+        HT[i] = NULL;
+
+    Insert(HT, 12);
+    Insert(HT, 22);
+    Insert(HT, 42);
+
+    temp = Search(HT[hash(22)], 22);
+
+    printf("%d\n", temp->data);
+
+    return 0;
+}
