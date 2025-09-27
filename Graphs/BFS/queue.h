317a8466658a883ba2995baf1cc7e1100a56b023
sava-stanchev <s.stanchev94@gmail.com>
2025-09-27 14:05:29 +0300

    graphs - bfs

---
+++ Graphs/BFS/queue.h
@@ -0,0 +1,57 @@
+#ifndef queue_h
+#define queue_h
+
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Node
+{
+    int data;
+    struct Node *next;
+} *front = NULL, *rear = NULL;
+
+void enqueue(int x)
+{
+    struct Node *t;
+    t = (struct Node *)malloc(sizeof(struct Node));
+
+    if (t == NULL)
+        printf("Queue is full\n");
+    else
+    {
+        t->data = x;
+        t->next = NULL;
+        if (front == NULL)
+            front = rear = t;
+        else
+        {
+            rear->next = t;
+            rear = t;
+        }
+    }
+}
+
+int dequeue()
+{
+    int x = -1;
+    struct Node *t;
+
+    if (front == NULL)
+        printf("Queue is empty\n");
+    else
+    {
+        x = front->data;
+        t = front;
+        front = front->next;
+        free(t);
+    }
+
+    return x;
+}
+
+int isEmpty()
+{
+    return front == NULL;
+}
+
+#endif
